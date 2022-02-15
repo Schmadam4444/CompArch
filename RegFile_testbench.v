04a4bcced1d860283c2f1e2167fcb5ca1884c303
Schmadam4444 <98973767+Schmadam4444@users.noreply.github.com>
2022-02-15 10:25:05 -0500

    Add files via upload

--- RegFile_testbench.v
+++ RegFile_testbench.v
@@ -17,7 +17,7 @@ module RegFile_testbench(rs1, rs2, readOut1, readOut2, rd, readEn, writeEn, data
 			rs1 <= 5'b00001; //source register 1 set to 0
 			rs2 <= 5'b00000; //source register 1 set to 0
 			dataIn <= 32'b00000000000000000000000000000000; //input data set to 0
-			#1280 $stop; //stop at time = 750
+			#3050 $stop; //stop at time = 750
 		end
 		
 		
@@ -25,21 +25,15 @@ module RegFile_testbench(rs1, rs2, readOut1, readOut2, rd, readEn, writeEn, data
 		
 		always begin
 			#10 dataIn <= $random; //random value for dataIn
-			#640 writeEn <= 1'b0;
-			#640 readEn <= 1'b1;
 			rd <= rd + 1; //destination register increment
+			#20 writeEn <= 1'b0;
+			#30 readEn <= 1'b1;
+			#30 writeEn <= 1'b1;
+			#20 readEn <= 1'b0;
 			rs1 <= rs1 + 1; //destination register increment
 			rs2 <= rs2 + 1; //destination register increment
-			#10; //delay
 		end
 		
-		wire [31:0] rf [31:0];
-		assign rf[0] = dut.rf[0];
-		assign rf[1] = dut.rf[1];
-		assign rf[2] = dut.rf[2];
-		assign rf[3] = dut.rf[3];
-		assign rf[4] = dut.rf[4];
-		assign rf[5] = dut.rf[5];
 		
 endmodule
 			
