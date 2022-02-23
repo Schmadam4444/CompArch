c22006ac7e9622222bd3645630443c99c54f0bde
Schmadam4444 <98973767+Schmadam4444@users.noreply.github.com>
2022-02-23 09:41:15 -0500

    Add files via upload

--- ALU.v
+++ ALU.v
@@ -50,50 +50,55 @@ module ALU (A, B, controlUnitIn, aluOut, carry, cOut, negative, zero, parity, ov
 				end	*/ 
 	
 			7'b0000011: // adder
-			
-				for (i=0; i < 32; i = i + 1) begin
+			;
+			/*	for (i=0; i < 32; i = i + 1) begin
 				
-					if (A[i] = 0 && B[i] = 0 && carry = 0) begin
+					if (A[i] == 0 && B[i] == 0 && carry == 0) begin
 						aluOut[i] <= 1'b0;
 						carry <= 1'b0;
 					end
 					
-					if((A[i] = 1 && B[i] = 0 && carry = 0)) || (A[i] = 0 && B[i] = 1 && carry = 0) || (A[i] = 0 && B[i] = 0 && carry = 1)) begin
+					if((A[i] == 1 && B[i] == 0 && carry == 0)) || (A[i] == 0 && B[i] == 1 && carry == 0) || (A[i] == 0 && B[i] == 0 && carry == 1)) begin
 						aluOut[i] <= 1'b1;
 						carry <= 1'b0;
 					end 
 					
-					if((A[i] = 1 && B[i] = 1 && carry = 0)) || (A[i] = 0 && B[i] = 1 && carry = 1) || (A[i] = 1 && B[i] = 0 && carry = 1)) begin
+					if((A[i] == 1 && B[i] == 1 && carry == 0)) || (A[i] == 0 && B[i] == 1 && carry == 1) || (A[i] == 1 && B[i] == 0 && carry == 1)) begin
 						aluOut[i] <= 1'b0;
 						carry <= 1'b1;
 					end 
 					
-					if (A[i] = 1 && B[i] = 1 && carry = 1) begin
+					if (A[i] == 1 && B[i] == 1 && carry == 1) begin
 						aluOut[i] <= 1'b1;
 						carry <= 1'b1;
 					end
 						
-				end
+				end */
 				
 			7'b0000100:
 			
+			;
 				//subtractor
 			
-			7'b0000101: //up sihfter (x2)
+			7'b0000101: //up shifter (x2)
 			
-				for (i = 30, i > 0, i--) begin
-					A[i] <= A[i-1];
+				/* for (i = 30; i > 0; i = i - 1) begin
+					A[i] <= A[i-32'b1];
 				end
 				
-				A[0] <= 0;
+				A[0] <= 0; */
+				
+				aluOut <= A << 1;
 				
 			7'b0000110: //down shifter (/2)
 			
-				for (i = 0, i < 30, i++) begin
-					A[i] <= A[i + 1];
+				/* for (i = 0; i < 30; i = i + 1) begin
+					A[i] <= A[i + 1'b1];
 				end
+				A[30] <= 0; */
 				
-				A[30] <= 0;
+				aluOut <= A >> 1;
+				
 			
 		endcase
 	
