85574d3ad4dd49edde7e2b77087e0ff57ac276c3
Frontman Dylan <98844974+FrontmanDylan@users.noreply.github.com>
2022-02-23 10:41:14 -0500

    Add files via upload

--- ALU.v
+++ ALU.v
@@ -1,109 +1,63 @@
-module ALU (A, B, controlUnitIn, aluOut, carry, cOut, negative, zero, parity, overflow);
+module ALU (A, B, opcode, O, cOut, negative, zero, parity, overflow, clk);
 	
 	input [31:0] A, B;
-	input [6:0] controlUnitIn;
-	output reg [31:0] aluOut;
-	output carry, cOut, negative, zero, parity, overflow;
-	integer i;
+	input [6:0] opcode;
+	input clk;
+	output reg [31:0] O;
+	output reg cOut, negative, zero, parity, overflow;
+	integer i, parityCount, signedFlag, sameSign;
+	 
+	always @(posedge clk) begin
 	
-	always @(*) begin
-	
-		case (controlUnitIn)
+		case (opcode)
 		
-			7'b0000000: //OR
+			7'b0000000:
 				
-				aluOut <= A | B;
-				
-				/* for (i=0; i < 32; i = i + 1) begin
+				O <= A | B; //OR
 					
-					aluOut[i] <= A[i] || B[i];
-					
-					/*
-					if (A[i] == && B[i] == 0)
-						aluOut[i] <= 0;
-					end
-					
-					else begin
-						aluOut[i] <= 1;
-					end
-				
-				end */
-					
-			7'b0000001: //AND
+			7'b0000001:
 			
-				aluOut <= A & B;
+				O <= A & B; //AND
 			
-				/* for (i=0; i < 32; i = i + 1) begin
-					
-					aluOut[i] <= A[i] && B[i];
-					
-				end	*/
-	
-			7'b0000010: //XOR
+			7'b0000010:
 			
-				aluOut <= A ^ B;
+				O <= A ^ B; //XOR
 			
-				/* for (i=0; i < 32; i = i + 1) begin
-					
-					aluOut[i] <= A[i] ^ B[i];
-					
-				end	*/ 
-	
-			7'b0000011: // adder
-			;
-			/*	for (i=0; i < 32; i = i + 1) begin
+			7'b0000011:
+			
+				O <= A + B; //ADD
 				
-					if (A[i] == 0 && B[i] == 0 && carry == 0) begin
-						aluOut[i] <= 1'b0;
-						carry <= 1'b0;
-					end
-					
-					if((A[i] == 1 && B[i] == 0 && carry == 0)) || (A[i] == 0 && B[i] == 1 && carry == 0) || (A[i] == 0 && B[i] == 0 && carry == 1)) begin
-						aluOut[i] <= 1'b1;
-						carry <= 1'b0;
-					end 
-					
-					if((A[i] == 1 && B[i] == 1 && carry == 0)) || (A[i] == 0 && B[i] == 1 && carry == 1) || (A[i] == 1 && B[i] == 0 && carry == 1)) begin
-						aluOut[i] <= 1'b0;
-						carry <= 1'b1;
-					end 
-					
-					if (A[i] == 1 && B[i] == 1 && carry == 1) begin
-						aluOut[i] <= 1'b1;
-						carry <= 1'b1;
-					end
-						
-				end */
 				
+					
+			/*
 			7'b0000100:
 			
-			;
 				//subtractor
 			
-			7'b0000101: //up shifter (x2)
+			7'b0000101:
 			
-				/* for (i = 30; i > 0; i = i - 1) begin
-					A[i] <= A[i-32'b1];
-				end
-				
-				A[0] <= 0; */
-				
-				aluOut <= A << 1;
-				
-			7'b0000110: //down shifter (/2)
-			
-				/* for (i = 0; i < 30; i = i + 1) begin
-					A[i] <= A[i + 1'b1];
-				end
-				A[30] <= 0; */
-				
-				aluOut <= A >> 1;
-				
-			
+				//shifter
+			*/
 		endcase
-	
+		
+		//NEGATIVE
+		negative <= (O[31] == 1)? 1'b1: 1'b0;
+		
+		//PARITY
+		parityCount = 0;
+		for (i = 0; i < 32; i = i + 1)
+			parityCount = (O[i] == 1)? parityCount + 1: parityCount;
+		
+			parity <= (parityCount % 2 == 0)? 1'b1: 1'b0;
+		
+		//OVERFLOW
+		signedFlag = (opcode == 7'b0000011)? 1'b1: 1'b0;
+		sameSign = (A[31] == B[31])? 1'b1: 1'b0;
+		overflow <= (signedFlag == 1'b1 && sameSign == 1'b1 && cOut == 1'b1)? 1'b1: 1'b0;
+		
+		//ZERO 
+		zero <= (O == 32'b0)? 1'b1: 1'b0;
+		
 	end
 	
 endmodule
-	
-	
