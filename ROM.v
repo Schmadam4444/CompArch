d451eb3950e2f274190cb77410b92cc1ac26f2ec
Adam <98973767+Schmadam4444@users.noreply.github.com>
2022-04-11 19:25:26 -0400

    Update ROM.v

--- ROM.v
+++ ROM.v
@@ -4,12 +4,6 @@ input [5:0] addr;
 reg[31:0] ROM[63:0];
 output reg [31:0] romOut;
 input en;
-integer i;
-
-	initial begin
-		for(i = 0; i < 64; i = i + 1)
-			ROM[i] <= i;
-	end
 
 	always @(*) begin
 	
